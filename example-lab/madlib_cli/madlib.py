f4e65bcee5d6aa99e6ed283e5d38b17a88abdb02
dutch foy <jdutchfoy@gmail.com>
2023-04-11 12:54:28 -0700

    resubmit per goback

--- example-lab/madlib_cli/madlib.py
+++ example-lab/madlib_cli/madlib.py
@@ -1,4 +1,6 @@
-# get your intro message
+import pytest
+
+# Define the intro function that prints an introduction message
 def intro():
     print("*" * 32)
     print("Welcome to the Mad Libs Game!")
@@ -7,21 +9,19 @@ def intro():
     print("*" * 32)
     print("")
 
-# Define your read template
-
-
+# Define the read_template function that reads the template file
 def read_template(filename):
     try:
-        # file???
+        # Open the file for reading
         with open(filename, "r") as template_file:
-            # remember the whitespace from last time
+            # Read the file contents and remove any leading/trailing whitespace
             template_content = template_file.read().strip()
             return template_content
     except FileNotFoundError:
-        # if I can’t locate raise an error
+        # If the file is not found, raise an error
         raise FileNotFoundError
 
-
+# Define the parse_template function that parses the template to get the number of inputs needed and the text to be displayed
 def parse_template(template):
     count = template.count("{")
     if count == 0:
@@ -31,27 +31,29 @@ def parse_template(template):
     parts = []
     stripped = ""
 
-    # Loop through the template and extract the info to show the amount of info to show
+    # Loop through the template and extract the text to be displayed and the number of inputs needed
     for bracket in range(count):
         # Find the index of the first "{" and "}"
         start = temp_template.index("{")
         end = temp_template.index("}")
 
-        # remove the info to be displayed and the amount of info needed
+        # Extract the text to be displayed and the number of inputs needed
         stripped = stripped + temp_template[0:start+1] + "}"
         parts.append(temp_template[(start+1):end])
 
+        # Remove the extracted text from the template
         temp_template = temp_template[(end+1):]
 
     # Add in the last part of the template after the last "}"
     stripped = stripped + temp_template
 
+    # Convert the list of parts to a tuple
     parts = tuple(parts)
 
+    # Return the stripped text and the tuple of parts
     return stripped, parts
+
 # Define the user_input function that prompts the user for input words
-
-
 def user_input(prompt):
     # Create an empty list to store the user input
     inputs = []
@@ -60,37 +62,43 @@ def user_input(prompt):
     for word_prompt in prompt:
         inputs.append(input(f"{word_prompt}: "))
 
+    # Return the list of user input
     return inputs
 
 # Define the merge function that generates the story based on the user input and the template
-
-
 def merge(stripped, words):
     new_story = stripped
 
     # Loop through the user input and replace the prompts with the user input
     for word in words:
-        index = new_story.index("{")
-        new_story = new_story[0:index] + word + new_story[index+1:]
+        new_story = new_story.replace("{}", word, 1)
 
     return new_story
 
 # Define the save_to_file function that saves the story to a file
-
-
 def save_to_file(story):
     with open("story.txt", "w") as f:
         f.write(story)
 
-# Define the main function that runs story
-
-
+# Define the main function that runs the game
 def main():
     # Print an introduction message
     intro()
-    template_content = read_template(
-        "../assets/dark_and_stormy_night_template.txt")
+
+    # Read the template file
+    template_content = read_template("../assets/dark_and_stormy_night_template.txt")
+
+    # Parse the template to get the number of inputs needed and the text to be displayed
     stripped, parts = parse_template(template_content)
+
+    # Prompt the user for input words
     words = user_input(parts)
-    # begin
+
+    # Generate the story based on the user input and the template
     new_story = merge(stripped, words)
+
+    # Save the story to a file
+    save_to_file(new_story)
+
+if __name__ == '__main__':
+    main()
